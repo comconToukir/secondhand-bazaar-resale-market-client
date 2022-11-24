adb7b1ac26bc00acbc95ab76eaabb8d1a54f545b
comconToukir <ahmdtoukir@gmail.com>
2022-11-24 12:43:12 +0600

    added firebase & userContext

---
+++ src/contexts/UserContext/UserContext.jsx
@@ -0,0 +1,69 @@
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
+import app from "../firebase/firebase.config";
+
+const auth = getAuth(app);
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const googleSignInProvider = new GoogleAuthProvider();
+
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleSignInProvider);
+  };
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const logInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOutUser = () => {
+    setLoading(true);
+    localStorage.removeItem("accessToken");
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const authInfo = {
+    googleSignIn,
+    createUser,
+    logInUser,
+    logOutUser,
+    user,
+    loading,
+  };
+
+  return (
+    <UserContext.Provider value={authInfo}>{children}</UserContext.Provider>
+  );
+};
+
+export default UserProvider;
